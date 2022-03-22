c35d62b98c1f6c683ae5a1e3ae16be069a26db33
186Casper186 <102165203+186Casper186@users.noreply.github.com>
2022-03-22 19:12:09 +0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,23 @@
+"RootType": "saveMetadataContainer",
+    "Data": {
+        "metadata": {
+            "gameDefinition": "",
+            "activeQuests": "6B577A51814903CA",
+            "trackedQuestEntry": "quests/main_quest/prologue/q001_01_victor/megabuilding/go_to_jackie",
+            "trackedQuest": "LocKey#9464",
+            "mainQuest": "LocKey#9464",
+            "debugString": "",
+            "locationName": "LocKey#44392",
+            "playerPosition": {
+                "X": -1381.680908203125,
+                "Y": 1263.386474609375,
+                "Z": 123.17665100097656
+            },
+            "playTime": 4116.6489301,
+            "playthroughTime": 4116.6489301,
+            "nextSavableEntityID": 9001652,
+            "nextNonSavableEntityID": 10007519,
+            "lifePath": "Nomad",
+            "bodyGender": "Male",
+            "brainGender": "Male",
+            "level": 2.0,
